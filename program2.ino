d817991739c186959899f033a7e380a01d03878f
Angel-panzerfaust <57466936+Angel-panzerfaust@users.noreply.github.com>
2020-10-27 16:10:24 -0600

    seccion de la pregunta 14

--- program2.ino
+++ program2.ino
@@ -2,12 +2,32 @@ include<Arduino.h>
 
 void setup()
 {
-	pinMode(5,OUTPUT);
-	
+	Serial.begin(9600)
+
+	//Todo bien? //simon prof
+	pinMode(3,OUTPUT);
+	// hola hola que tal, hay algun conflicto? //jaja si pero en iraq
+
+	//Todo bien?
+	pinMode(3,OUTPUT);
+	// hola hola que tal, hay algun conflicto?
+
+	pinMode(6,OUTPUT);
 }
 void loop()
 {
-	digitalWrite(5,HIGH);
+	//se altera el codigo de esta parte
+	//digitalWrite(5,HIGH);
 	digitalWrite(5,LOW);
-}
+	//fin de las modificaciones p1
+
+	delay(1000);
+	analogWrite(3,250);
+	Serial.print("hola");
+	delay(220);
+	analogRead(10,102);
+	//modificaiones pt13
+	Serial.print("angel tiene un 10");
+	Serial.println("yes/yes");
+
 
--- programa1.cpp
+++ programa1.cpp
@@ -1,10 +1,29 @@
 
+
+
 #include<stdio.h>
 #include<conio.h>
 
 void main()
 {
- int c=0;
- int f=1;
+ char c="";
+ int i=0;
+ int j=1;
  //hasta aqui es la modificacion de prog 1
-}
+ float x=4.5;
+ printf("Aqui toy");
+
+ printf("hola Mundo");
+ printf("yo tengo estres");
+ printf("Please help");
+ //modificaciones de la preg 13
+ c=a+b;
+ a=c*b;
+ printf("esto me suena :\");
+
+ for(i=0:i<4:i++){
+
+	 printf("hola Mundo");
+ 	printf("yo tengo estres");
+ 	printf("Pleace help");
+ }
