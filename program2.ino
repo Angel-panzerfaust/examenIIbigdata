61ac365f0bb06a5f992e7cabf4239afe82a79951
Angel-panzerfaust <57466936+Angel-panzerfaust@users.noreply.github.com>
2020-10-27 13:16:11 -0600

    segunda modificacion

--- program2.ino
+++ program2.ino
@@ -9,5 +9,10 @@ void loop()
 {
 	digitalWrite(5,HIGH);
 	digitalWrite(5,LOW);
+	//fin de las modificaciones p1
+
+	delay(500);
+	analogWrite(10,234);
+	analogRead(10,100);
 }
 
--- programa1.cpp
+++ programa1.cpp
@@ -7,4 +7,9 @@ void main()
  int c=0;
  int f=1;
  //hasta aqui es la modificacion de prog 1
+ float x=4.5;
+ printf("hola mundo");
+ printf("yo tengo estres");
+ printf("plz help");
+
 }
