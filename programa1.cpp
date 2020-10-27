9cbcc7f1d9c0b0b1f8459f741ca86ceba5459de1
cboyain <5940044+cboyain@users.noreply.github.com>
2020-10-27 15:24:39 -0600

    Nuevos cambios realizados por el docente para que se integren a las modificaciones que tiene el alumno

--- programa1.cpp
+++ programa1.cpp
@@ -4,14 +4,16 @@
 
 void main()
 {
+ char c="";
  int i=0;
  int j=1;
  //hasta aqui es la modificacion de prog 1
  float x=4.5;
  printf("Aqui toy");
- printf("hola Mundo");
- printf("yo tengo estres");
- printf("Pleace help");
-
+ for(i=0:i<4:i++){
+	 printf("hola Mundo");
+ 	printf("yo tengo estres");
+ 	printf("Pleace help");
+ }
 
 }
